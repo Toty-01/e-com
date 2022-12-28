3b1b852efb96576b30e25d9c1cf464ba0e85b19e
Toty-01 <caucheanthony@gmail.com>
2022-12-28 18:56:50 +0100

    filters bar ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,14 @@ import { MatTableModule } from '@angular/material/table';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsHeaderComponent
   ],
   imports: [
     BrowserModule,
