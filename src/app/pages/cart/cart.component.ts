dddf8d4dfd123ee75498e805a6a306a4de380af6
Toty-01 <caucheanthony@gmail.com>
2022-12-31 01:13:32 +0100

    in a good way

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -37,10 +37,30 @@ export class CartComponent implements OnInit {
 
   ngOnInit():void {
     this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
 
   getTotal(items: Array<CartItem>): number {
    return this.cartService.getTotal(items);
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
   }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -26,6 +26,25 @@ export class CartService {
     
   }
 
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id) {
+          _item.quantity--;
+        if(_item.quantity === 0) {
+            itemForRemoval = _item;
+         }
+      }   
+      return _item;
+    })
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval, false);
+    }
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open("L'article à été correctement supprimé !", 'Ok', {duration: 3000});
+  }
+
   getTotal(items: Array<CartItem>): number {
     return items.map((item) => item.price * item.quantity)
     .reduce((prev, current) => prev + current, 0);
@@ -35,4 +54,15 @@ export class CartService {
     this.cart.next ({ items: []});
     this._snackBar.open('Le panier à été vidé avec succès !', 'Ok', { duration: 3000 });
    }
+
+   removeFromCart(item: CartItem, update = true): Array<CartItem> {
+     const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id)
+
+      if(update) {
+        this.cart.next({items: filteredItems});
+        this._snackBar.open("Les articles ont été correctement supprimés !", 'Ok', {duration: 3000});
+      }
+      return filteredItems;
+   };
 }
